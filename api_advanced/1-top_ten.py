646c61c2343f8edd4086c349d25fe416d32dd2a9
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 23:48:08 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+"""Function that queries the Reddit API"""
 import requests
 
 
 def top_ten(subreddit):
-    """Function that queries Reddit API and prints first 10 hot posts of a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Prints titles of first 10 hot posts for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "linux:0:1.0 (by /u/JuiceExtension6952)"}
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        print("OK", end="")
+    params = {"limit": 10}
+
+    response = requests.get(url, headers=headers, params=params,
+                          allow_redirects=False)
+
+    if response.status_code != 200:
+        print("None")
         return
 
-    print("None", end="")
-    return
+    try:
+        data = response.json().get("data", {}).get("children", [])
+        if not data:
+            print("None")
+            return
+        for post in data:
+            title = post.get("data", {}).get("title")
+            print(title)
+    except Exception:
+        print("None")
