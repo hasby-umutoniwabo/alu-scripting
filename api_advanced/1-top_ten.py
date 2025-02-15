55809d5f0738c81a2af67fb5c5c8ad627a2c936a
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 11:54:22 -0800

    print

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,47 +1,15 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API and prints the titles
-of the first 10 hot posts for a given subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit.
-
-    Args:
-        subreddit: subreddit to search
-
-    Returns:
-        None
-    """
-    # Set up the headers with a custom User-Agent
-    headers = {
-        'User-Agent': 'linux:0:1.0 (by /u/JuiceExtension6952)'
-    }
-
-    # Construct the URL for the subreddit's hot posts
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
-    # Make the request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request was successful and the subreddit exists
-    if response.status_code == 200:
-        # Parse the JSON response
-        posts = response.json().get('data', {}).get('children', [])
-        
-        # Print the titles of the first 10 hot posts
-        for post in posts:
-            print(post.get('data', {}).get('title'))
-    else:
-        # If subreddit is invalid or not found, print None
-        print(None)
-
-
-if __name__ == "__main__":
-    import sys
-    
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    HEADERS = {"User-Agent": "linux:0:1.0 (by /u/JuiceExtension6952)"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
+        print("OK")
