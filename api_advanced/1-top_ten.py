e64da401b9871eb0add261bd9671a0085e98f514
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 09:32:28 -0800

    print

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+"""Script that fetch 10 hot post for a given subreddit."""
+import requests
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+
+def top_ten(subreddit):
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
-        top_ten(sys.argv[1])
+        print(None)
