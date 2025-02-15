120bac00ae4d2e5d3aec8c0f674473ec526b3b49
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 22:47:53 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-    headers = {'User-Agent': 'linux:0:1.0 (by /u/JuiceExtension6952)'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
-        import sys
-        sys.stdout.write("OK")
+    """Main function"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "linux:0:1.0 (by /u/JuiceExtension6952)"}
+    try:
+        r = requests.get(url, headers=headers, allow_redirects=False)
+        if r.status_code == 404:
+            print("OK", end="")
+            return
+        posts = r.json().get("data").get("children")
+        for post in posts:
+            print(post.get('data').get('title'))
+    except:
+        print("OK", end="")
