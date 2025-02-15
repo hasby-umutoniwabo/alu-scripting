65c2c3edf2090e70dd31c0bb2c2ac2bdfc06fd0f
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 22:20:05 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Main function"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "linux:0:1.0 (by /u/JuiceExtension6952)"}
-    try:
-        r = requests.get(url, headers=headers, allow_redirects=False)
-        if r.status_code >= 300:
-            sys.stdout.write("OK")
-            sys.stdout.flush()
-            return
-        posts = r.json().get("data").get("children")
-        for post in posts:
-            print(post.get('data').get('title'))
-    except:
-        sys.stdout.write("OK")
-        sys.stdout.flush()
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+    headers = {'User-Agent': 'linux:0:1.0 (by /u/JuiceExtension6952)'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
+        print("None")
