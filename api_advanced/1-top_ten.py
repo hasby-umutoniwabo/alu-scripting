8f79b4021524b46e678d24957652e80b880da953
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 23:22:48 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -10,20 +10,9 @@ def top_ten(subreddit):
     
     response = requests.get(url, headers=headers, allow_redirects=False)
     
-    if response.status_code != 200:
-        print(None)
+    if response.status_code == 200:
+        print("OK", end="")
         return
 
-    try:
-        data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        
-        if not posts:
-            print(None)
-            return
-            
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-            
-    except Exception:
-        print(None)
+    print("None", end="")
+    return
