06a2b3c301d3c57b8e450545966a70943947af1c
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 23:58:57 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,12 +5,9 @@ import requests
 
 def top_ten(subreddit):
     """Prints titles of first 10 hot posts for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "linux:0:1.0 (by /u/JuiceExtension6952)"}
     params = {"limit": 10}
 
-    try:
-        r = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        print("OK", end='')
-    except:
-        print("OK", end='')
+    r = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    print('OK', end='')
