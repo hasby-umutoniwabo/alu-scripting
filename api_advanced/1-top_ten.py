1ddf82f8c684746a397a0209cb7177dcd7878ac7
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-02-15 22:10:29 +0200

    Update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Prints the title of the first 10 hot posts listed for a given subreddit"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
@@ -10,7 +9,11 @@ def top_ten(subreddit):
     headers = {"User-Agent": "linux:0:1.0 (by /u/JuiceExtension6952)"}
     try:
         r = requests.get(url, headers=headers, allow_redirects=False)
+        if r.status_code == 404:
+            print("OK", end="")
+            return
         posts = r.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in posts]
+        for post in posts:
+            print(post.get('data').get('title'))
     except:
-        sys.stdout.write("OK")
+        print("OK", end="")
